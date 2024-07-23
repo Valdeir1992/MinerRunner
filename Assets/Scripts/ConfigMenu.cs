3f355099e7b05187deee2bb49493fecc790897dd
Marina Evangelista <175274813+MariEvan@users.noreply.github.com>
2024-07-23 10:18:28 -0300

    Atualizações
    Ainda não está pronto

--- Assets/Scripts/ConfigMenu.cs
+++ Assets/Scripts/ConfigMenu.cs
@@ -2,42 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
+using Unity.Audio;
+using static UnityEditor.Recorder.OutputPath;
 
 public class ConfigMenu : MonoBehaviour
 {
-    public Slider fill;
-    
-    //Botão (NÃO ESTÁ FUNCIONANDO - "objeto instantiate é nulo")
-    private void Awake()
+    //Slider Volume
+
+    public void SetVolume (float volume)
     {
-        var root = GetComponent<UIDocument>().rootVisualElement;
-        var returnButton = root.Q<Button>("BT_Voltar");
-        returnButton.clicked += () => Destroy(gameObject);
+        Debug.Log(volume);
     }
 
-    //Slider e Fill Bar (NÃO ESTÁ FUNCIONANDO)
 
-    private VisualElement m_root;
-    private VisualElement m_slider;
-    private VisualElement m_dragger;
-    private VisualElement m_bar;
-
-    void Start()
-    {
-        m_root = GetComponent<UIDocument>().rootVisualElement;
-        m_slider = m_root.Q<VisualElement>("VolumeMusica");
-        m_dragger = m_root.Q<VisualElement>("unity-dragger");
-
-        AddElements();
-     
-    }
-
-    void AddElements()
-    {
-        m_bar = new VisualElement();
-        m_dragger.Add(m_bar);
-        m_bar.name = "Bar";
-        m_bar.AddToClassList("bar");
-    }
 
 }
--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -7,12 +7,11 @@ using UnityEngine.SceneManagement;
 public class MenuScript : MonoBehaviour
 {
     [SerializeField] private ConfigMenu _startMenuPrefab;
-
-    private void Awake()
+    private void Start()
     {
         VisualElement root = GetComponent<UIDocument>().rootVisualElement;
 
-        root.Q<Button>("BT_Config").clicked += async () => InstantiateAsync(_startMenuPrefab);
+        root.Q<Button>("BT_Config").clicked += async () => InstantiateAsync(_startMenuPrefab); //NÃO FUNCIONA 
         root.Q<Button>("BT_Play").clicked += () => SceneManager.LoadSceneAsync(1);
        
     }
