648a5eef4ad0453be8c2b6bd987e383bc253ff4d
WalkerAway <marcantoniosantosilva@gmail.com>
2024-07-24 20:40:42 -0300

    Terminado o swipe/movimentação do player

--- Assets/Scripts/SwipeControls.cs
+++ Assets/Scripts/SwipeControls.cs
@@ -2,27 +2,40 @@
 
 public class SwipeMovement : MonoBehaviour
 {
-    public float moveSpeed = 10f;
     public SpawnManager spawnManager;
-
-    private void Start()
-    {
-
-    }
+    private int desiredLane = 1; // 0 = esquerda, 1 = meio, 2 = direita
+    public float laneDistance = 4f; // Distância entre as lanes
 
     private void Update()
     {
-        float hMoviment = Input.GetAxis("Horizontal") * moveSpeed / 2;
-        float vMoviment = Input.GetAxis("Vertical") * moveSpeed;
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            desiredLane++;
+            if (desiredLane == 3)
+            {
+                desiredLane = 2;
+            }
+        }
 
-        transform.Translate(new Vector3(hMoviment,0, vMoviment) * Time.deltaTime);
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            desiredLane--;
+            if (desiredLane == -1)
+            {
+                desiredLane = 0;
+            }
+        }
+
+        // Calcular a nova posição com base na lane desejada
+        Vector3 targetPosition = transform.position;
+        targetPosition.x = (desiredLane - 1) * laneDistance;
+
+        // Atualizar a posição do jogador
+        transform.position = targetPosition;
     }
 
     private void OnTriggerEnter(Collider other)
     {
         spawnManager.TriggerEntered();
     }
-
-
-
 }
