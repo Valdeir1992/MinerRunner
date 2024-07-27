b17ee4380c7e76e4f6c9d1bbc21be1b8ada2dff3
Valdeir Antonio do Nascimento <valdeir.ans@gmail.com>
2024-07-27 18:49:29 -0300

    update

--- Assets/Scripts/SwipeControls.cs
+++ Assets/Scripts/SwipeControls.cs
@@ -1,31 +1,53 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class SwipeMovement : MonoBehaviour
 {
     public SpawnManager spawnManager;
+    private GameInputs _gameinputs;
     private int desiredLane = 1; // 0 = esquerda, 1 = meio, 2 = direita
     public float laneDistance = 4f; // Distância entre as lanes
 
-    private void Update()
+    private void Start()
     {
-        if (Input.GetKeyDown(KeyCode.RightArrow))
+        _gameinputs = new GameInputs();
+        _gameinputs.Gameplay.Enable();
+        _gameinputs.Gameplay.Right.started += MoveToRight;
+        _gameinputs.Gameplay.Left.started += MoveToLeft;
+    }
+    /// <summary>
+    /// Move o personagem para esqueda.
+    /// </summary>
+    /// <param name="context">Receve o contexto do InputSystem</param>
+    /// <exception cref="NotImplementedException"></exception>
+    private void MoveToLeft(InputAction.CallbackContext context)
+    {
+        desiredLane--;
+        if (desiredLane == -1)
         {
-            desiredLane++;
-            if (desiredLane == 3)
-            {
-                desiredLane = 2;
-            }
+            desiredLane = 0;
         }
-
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        Debug.Log("Esquerda");
+    }
+    /// <summary>
+    /// Move o personagem para direita.
+    /// </summary>
+    /// <param name="context">Recebe o contexto do InputSystem</param>
+    /// <exception cref="NotImplementedException"></exception>
+    private void MoveToRight(InputAction.CallbackContext context)
+    {
+        desiredLane++;
+        if (desiredLane == 3)
         {
-            desiredLane--;
-            if (desiredLane == -1)
-            {
-                desiredLane = 0;
-            }
+            desiredLane = 2;
         }
+        Debug.Log("Direita");
+    }
 
+    private void Update()
+    { 
+         
         // Calcular a nova posição com base na lane desejada
         Vector3 targetPosition = transform.position;
         targetPosition.x = (desiredLane - 1) * laneDistance;
