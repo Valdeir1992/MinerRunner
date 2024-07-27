30a036175c4113761e06f335f99472f1c3c4a707
Valdeir Antonio do Nascimento <valdeir.ans@gmail.com>
2024-07-27 16:59:58 -0300

    update

--- Assets/Scripts/StartMenu/MenuScript.cs
+++ Assets/Scripts/StartMenu/MenuScript.cs
@@ -1,23 +1,3 @@
-<<<<<<< HEAD:Assets/Scripts/StartMenu/MenuScript.cs
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UIElements;
-using UnityEngine.SceneManagement;
-
-public class MenuScript : MonoBehaviour
-{
-    [SerializeField] private StartMenuConfiguration _startMenuPrefab;
-    private void Awake()
-    {
-        VisualElement root = GetComponent<UIDocument>().rootVisualElement;
-
-        root.Q<Button>("BT_Config").clicked += async () => InstantiateAsync(_startMenuPrefab);
-        root.Q<Button>("BT_Play").clicked += () => SceneManager.LoadSceneAsync(2);
-       
-    }
-}
-=======
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -35,5 +15,4 @@ public class MenuScript : MonoBehaviour
         root.Q<Button>("BT_Play").clicked += () => SceneManager.LoadSceneAsync(1);
        
     }
-}
->>>>>>> ui:Assets/Scripts/MenuScript.cs
+} 
