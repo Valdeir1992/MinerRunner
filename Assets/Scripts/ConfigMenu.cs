538277d72451844bfa5d6b9a77b648865c4295f3
Valdeir Antonio do Nascimento <valdeir.ans@gmail.com>
2024-07-27 18:28:10 -0300

    update

--- Assets/Scripts/ConfigMenu.cs
+++ Assets/Scripts/ConfigMenu.cs
@@ -4,11 +4,36 @@ using UnityEngine;
 using UnityEngine.UIElements;
 using Unity.Audio;
 using static UnityEditor.Recorder.OutputPath;
+using System;
 
 public class ConfigMenu : MonoBehaviour
 {
     //Slider Volume
+    private void Start()
+    {
+        var root = GetComponent<UIDocument>().rootVisualElement;
+        var slider = root.Q<Slider>();
+        SetFillSlider(slider);
+    }
 
+    /// <summary>
+    /// Adicionar componente de arrastar visualmente no slider
+    /// </summary>
+    /// <param name="volume"></param>
+    private void SetFillSlider(Slider slider)
+    {
+        var container = new VisualElement();
+        container.name = "Visual fill";
+        container.style.position = Position.Absolute;
+        var bar = new VisualElement();
+        container.Add(bar);
+        slider.Add(container);
+        slider.RegisterValueChangedCallback<float>(ctx => 
+        {
+            bar.style.width = Length.Percent(ctx.newValue);
+            Debug.Log(ctx.newValue);
+        });
+    }
     public void SetVolume (float volume)
     {
         Debug.Log(volume);
