20d19f1b0299a89852873723ef88cb6ccbca9678
Marina Evangelista <marinaevangelista303@gmail.com>
2024-10-15 19:52:38 -0300

    FMOD no projeto

--- Assets/Scripts/StartMenu/MenuScript.cs
+++ Assets/Scripts/StartMenu/MenuScript.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 
 public class MenuScript : MonoBehaviour
 {
-    [SerializeField] private ConfigMenu _startMenuPrefab;
+    [SerializeField] ConfigMenu _startMenuPrefab;
 
     private void Awake()
     {
@@ -19,8 +19,8 @@ public class MenuScript : MonoBehaviour
 
         root.Q<Button>("BT_Config").clicked += async () =>
         {
-            InstantiateAsync(_startMenuPrefab);
             UIAudioManager.instance.PlayOneShot(UIFMODEvents.instance.buttonSFX, this.transform.position); //Tocar som do botao
+            InstantiateAsync(_startMenuPrefab);
         };
 
         root.Q<Button>("BT_Play").clicked += () => 
